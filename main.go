2a3be5af3b22af107aa8affb735eddbca160e60c
s-yuk <100174001+s-yuk@users.noreply.github.com>
2023-02-18 13:27:33 +0900

    mysqlへ接続できました

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func init() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	Db = db
+}
 
 func main() {
 	fmt.Printf("%s", "hello, world\n")
